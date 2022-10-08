6bbdda7c20535e31ee8b4a2298fba641f3d6efe8
vincenttommi <vincenttommi@gmail.com>
2022-10-08 11:32:59 +0300

    assigning one value to multiple variables

--- vin.py
+++ vin.py
@@ -1,8 +1,5 @@
-# Assiging multiple values to variables in python
-x,y,z = "Django","Flask","Pyramind"
-
+# Assiging one value to multiple variables 
+x=y=z="vincenttommi you can become the best dev if you put in consitency"
 print(x)
 print(y)
 print(z)
-
-
