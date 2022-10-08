d7f84a87a482ddf656d2eaab7456cb6741408294
vincenttommi <vincenttommi@gmail.com>
2022-10-08 12:31:20 +0300

    more global variables in python

--- vin.py
+++ vin.py
@@ -1,14 +1,13 @@
 # global variables 
 # created outside a function and can be accessed both outside and inside the function
-x  = "awesome"
+# in above two variables are created one outside using the same name and another inside 
+# accesed in the same function but bring different outcome
+x = "progamming language"
 
-def myfunc():
-    x  = "is  cool"
-    print("python is "  + x)
-   
-myfunc() 
-print("coding python is " + x)    
+def myfunction():
+    print("python is an easier" + x)
     
-    
+myfunction()
+print("Learning a " + x)    
 
 
