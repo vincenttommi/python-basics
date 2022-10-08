1a21f0be289ffbde4ec18a4effd26a4dcb582cac
vincenttommi <vincenttommi@gmail.com>
2022-10-08 13:09:59 +0300

    changing global variables inside a function using global key

--- vin.py
+++ vin.py
@@ -1,17 +1,12 @@
 #global keyword
 # used for creating a global variable inside a function
+# changing value of global keyword inside a function  we refer to the variable  by using globalkeyword
 
+x = "cool"
 
+ 
 def myfunction():
     global x 
-    x =  "cool programming langauge"
- 
+    x = "great"
 myfunction()
-print("python is a " + x)    
-    
-      
-    
-
-
-
-
+print("python is " + x)    
