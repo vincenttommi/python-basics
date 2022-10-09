27031f8230a35fd92addf592cc506edfcfb16d1f
vincenttommi <vincenttommi@gmail.com>
2022-10-09 13:34:40 +0300

    floating  numbers in python

--- vin.py
+++ vin.py
@@ -1,9 +1,15 @@
 # python numbers , there are three int float and complex
 # variables of numeric types are created when we assign values to them
+# float numbers in python either positive or negative and containing one or more decimals
 
-x = 3
-y = 2345678904
-z = -3334678
+
+x = 1.0
+y = 34.67
+z = -23.0
+
+print(type(x))
+print(type(y))
+print(type(z))
 
 print(x)
 print(y)
