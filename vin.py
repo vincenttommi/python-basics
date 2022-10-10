36898d6461d1ef00b1379b2e189254f9bed07895
vincenttommi <vincenttommi@gmail.com>
2022-10-10 12:54:21 +0300

    slicing from the end

--- vin.py
+++ vin.py
@@ -2,7 +2,7 @@
 #obtaining a substring from a given string starting from start to end.
 #we specifty the start and the end with :  to get the outcome.
 
-#slicing from the start
+#slicing from the send
 
 x = "vincenttommi"
-print(x[:4])
+print(x[:11])
