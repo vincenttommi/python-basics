8908651026b25cd18712cf7473618e14ad422f40
vincenttommi <vincenttommi@gmail.com>
2022-10-10 11:35:34 +0300

    len() printing length of a string in python

--- vin.py
+++ vin.py
@@ -1,10 +1,5 @@
-# strings in python
-# strings in python are sorrounded by either double qoutes or single qoutation marks
-# strings are arrays in  python
+#len() returns the lenght of a string in python
 
-
-# Accessing index numbers of characters  in string elements
-
-name = "vincenttommi"
-
-print(name[11])
+x = "rongai"
+print(len(x))
+ 
