8e33dd7a3369468e70b0d15e6a4df46cf59c7710
vincenttommi <vincenttommi@gmail.com>
2022-10-10 10:48:04 +0300

    assiging a string to a variable

--- vin.py
+++ vin.py
@@ -1,17 +1,7 @@
-#casting in python
-# Done in python using constructor functions()
-# used to specify a type 0f variable  declared in a program
-# int() constructs an integer number from integral literal or float
-# float constructs an a float froma an integral literal , float literal or string
-# str() constructs a string from a wide variety of data types,including strings,integer literals and float literal
+# strings in python
+# strings in python are sorrounded by either double qoutes or single qoutation marks
 
-#str()
-# converts integer and float literals to strings
 
-x = str("s34")
-y = str(2.03)
-z = str(17)
-
+# assigning a string to a variable
+x = "vincenttommi"
 print(x)
-print(y)
-print(z)
