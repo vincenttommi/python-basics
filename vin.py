53aa5f011c41c0e3e9d14d992472f916e0f472f1
vincenttommi <vincenttommi@gmail.com>
2022-10-10 10:36:20 +0300

    casting a variable using float()

--- vin.py
+++ vin.py
@@ -5,11 +5,12 @@
 # float constructs an a float froma an integral literal , float literal or string
 # str() constructs a string from a wide variety of data types,including strings,integer literals and float literal
 
-# int()
-#converts the numbers to integers
-x = (23)
-y = (23.45)
-z = ("34.56")
+#float()
+# converts integer and string literals to float
+
+x = float(23)
+y = float("34")
+z = float(17)
 
 print(x)
 print(y)
