d6c28496bd8711c9db07c3d9292124a2e67eb60d
vincenttommi <vincenttommi@gmail.com>
2022-10-10 12:03:38 +0300

    checking if a string is absent in a statement or phrase

--- vin.py
+++ vin.py
@@ -1,6 +1,8 @@
 #checking presence of a string in python using if statement
+#cheking if a certain character is absent  or presence in a statement
+# we use the keyword  not in
+#check if NOT
 
-name = "vincenttommmikorir"
+name = "I schooled in majengo primary school for my primary  school education"
+print("majengo"  not in name)
 
-if "vincent" in name:
-    print("true,'its my name niga'")
