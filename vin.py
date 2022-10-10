347ee0e99734333455673b9d0b5e851a53519c72
vincenttommi <vincenttommi@gmail.com>
2022-10-10 10:29:46 +0300

    int() casting in python

--- vin.py
+++ vin.py
@@ -1,8 +1,16 @@
-# random number in python
-# built in model used to make random numbers
+#casting in python
+# Done in python using constructor functions()
+# used to specify a type 0f variable  declared in a program
+# int() constructs an integer number from integral literal or float
+# float constructs an a float froma an integral literal , float literal or string
+# str() constructs a string from a wide variety of data types,including strings,integer literals and float literal
 
+# int()
+#converts the numbers to integers
+x = (23)
+y = (23.45)
+z = ("34.56")
 
-import random 
-
-print(random.randrange(1000,5000))
-
+print(x)
+print(y)
+print(z)
