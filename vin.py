47a691fa4bd9691abbc37117fb13490ac9329f22
vincenttommi <vincenttommi@gmail.com>
2022-10-12 01:50:29 +0300

    escape characters in python

--- vin.py
+++ vin.py
@@ -1,15 +1,5 @@
-#string formats
-#we comnine numbers and string using  format()
-#inserts numbers into strings
-#Takes unlimited number of arguments and replaces it repectively
+#escape character in python
+# are used to insert characters that are illegal in a string
 
-
-
-
-
-quantity = 3
-itemno = 537
-price = 50.34
-myorder = "I want to pay  {2} dollars for {0}pieces of item {1}. "
-print(myorder.format(quantity,itemno,price))
-
+x = "you can become a great developer if you put \"more work\" thats all vin"
+print(x)
