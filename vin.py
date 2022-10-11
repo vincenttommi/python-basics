2c5f349ea99f72fe80b048e53e0ba2aeac1839e1
vincenttommi <vincenttommi@gmail.com>
2022-10-12 00:46:38 +0300

    strip() removes space from a string

--- vin.py
+++ vin.py
@@ -1,7 +1,7 @@
 #modifying strings in python
 #has a set of built-in methods to modify sets.
 
-# name()  returnsstring in lowercase
+# strip() removes the whitespace from a function
 
-name = "vincent tommi"
-print(name.lower())
+name = " vincent tommi "
+print(name.strip())
