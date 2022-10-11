abb64885bceb795d53a1fda7910f4ab6ea4812ec
vincenttommi <vincenttommi@gmail.com>
2022-10-12 01:53:42 +0300

    more escape characters

--- vin.py
+++ vin.py
@@ -1,5 +1,7 @@
 #escape character in python
 # are used to insert characters that are illegal in a string
 
-x = "you can become a great developer if you put \"more work\" thats all vin"
-print(x)
+# x = "you can become a great developer if you put \"more work\" thats all vin"
+# print(x)
+message = 'it\'s great'
+print(message)
