28fe74e42983ab2a1c79e33100c1537ebb086b52
vincenttommi <vincenttommi@gmail.com>
2022-10-12 01:41:46 +0300

    more format()

--- vin.py
+++ vin.py
@@ -1,7 +1,15 @@
 #string formats
 #we comnine numbers and string using  format()
+#inserts numbers into strings
+#Takes unlimited number of arguments and replaces it repectively
 
-age = "23"
 
-text = "vincent is {} years old"
-print(text.format(age))
+
+
+
+quantity = 3
+itemno = 537
+price = 50.34
+myorder = "I want to pay  {2} dollars for {0}pieces of item {1}. "
+print(myorder.format(quantity,itemno,price))
+
