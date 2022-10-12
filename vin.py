c7d4d48e7c979e5de6f052d0eaea1ba5edd47cec
vincenttommi <vincenttommi@gmail.com>
2022-10-13 02:54:38 +0300

    assigmnet operator in python

--- vin.py
+++ vin.py
@@ -1,17 +1,4 @@
-#python operators and values are used to perform operations values and  variables
-
-
-#division of operators in python
-#bitwise operators
-#Assignment operators
-#comparison operators
-#Logical operators
-#Identity operators
-#Membership operators
-#Bitwise operators
-
-x = 1500
-y = 2
-
-print(x//y)
-#Rounds the numbers down to the  nearest whole number
+#assignment operators
+#are used to assign  values to operators
+x = 5
+print(x)
