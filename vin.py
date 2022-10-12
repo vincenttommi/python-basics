6c3979312fb0cf2ed1c7faad96fde4474900e284
vincenttommi <vincenttommi@gmail.com>
2022-10-13 02:01:05 +0300

    more boolean values

--- vin.py
+++ vin.py
@@ -1,9 +1,12 @@
 #Evaluation of values  and variables
 #evaluation of values using bool() returns true or false
 
-print(bool(False))
-print(bool(None))
-print(bool(0))
-print(bool(""))
-print(bool([]))
-print(bool({}))
+# class myclass():
+#     def _len_self():
+#         return 0
+# myobj = myclass()
+# print(bool(myobj))    
+#functions can return a boolean value
+def myfunction():
+    return False
+print(myfunction())
