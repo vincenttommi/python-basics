18a02e2e3537b2e8595f7dc9b4248443f6122e82
vincenttommi <vincenttommi@gmail.com>
2022-10-13 03:44:22 +0300

    identity operators in python

--- vin.py
+++ vin.py
@@ -1,5 +1,13 @@
-#logical operators in python
-#are used to combine  conditional statements
+#python identity operators
+#are used to compare objects  not if they are equal but if they are actually the same with same memory location
+x = ["apple","banana"]
+y = ["apple","banana"]
 
-x = 5
-print(x > 3  and x < 20)
+z = x
+
+print( x is z)
+#returns true because x is the same object as  z
+print( x is y)
+#returns false because x is  not the same object as y ,yet they have the same values
+print(x == y)
+#Demonstrates difference btn "is" and "=" returns true since its equal to y
