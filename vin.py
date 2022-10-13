3329373b9438ddeb84e665d5426e57646d36526a
vincenttommi <vincenttommi@gmail.com>
2022-10-13 03:48:28 +0300

    membership operator in python

--- vin.py
+++ vin.py
@@ -1,13 +1,5 @@
-#python identity operators
-#are used to compare objects  not if they are equal but if they are actually the same with same memory location
-x = ["apple","banana"]
-y = ["apple","banana"]
+#python membership operators
+#are used to test if a sequence is true in an object
+x  = ["vincent","tommi"]
 
-z = x
-
-print( x is z)
-#returns true because x is the same object as  z
-print( x is y)
-#returns false because x is  not the same object as y ,yet they have the same values
-print(x == y)
-#Demonstrates difference btn "is" and "=" returns true since its equal to y
+print("tommi" in x)
