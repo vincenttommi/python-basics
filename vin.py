f7466f3aae33a8b1fcc9b3ab4c9cff59367f10e3
vincenttommi <vincenttommi@gmail.com>
2022-10-13 04:13:36 +0300

    list item datatypes

--- vin.py
+++ vin.py
@@ -1,3 +1,13 @@
 #python list are used to store multiple items in a single variable
-names = list["vincent","tommi","korir"]
-print(names)
+#len() used to determine  how many items a list  has
+#can be of any datatype string int boolean
+
+#list items - Data types
+
+list1 = ["apple","banana","cherry"]
+list2 = [1,2,3,4,5,6,7]
+list3 = [True,False,True]
+
+print(list1)
+print(list2)
+print(list3)
