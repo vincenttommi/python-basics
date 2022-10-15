c6e94837ddfa325ceb1ad72178f746620b73fce3
vincenttommi <vincenttommi@gmail.com>
2022-10-15 10:03:37 +0300

    checking if it exist in this list

--- vin.py
+++ vin.py
@@ -1,9 +1,4 @@
-#range of indexes
-#you can specifty range of indexes by speciftying where to start and where to end
-#when specifying a range the return value will be  a  new  list with  specified items
+#checking if item exits in list 
 
-thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
-print(thislist[2:])
-#the search will start at index 2 and end at index 5
-
-
+thislist = ["apple","mango","banana","cherry"]
+print("yes, its presence in this list")
