4d4c21e5846bab7e79fdd6dffd75343af7cb0465
vincenttommi <vincenttommi@gmail.com>
2022-10-15 09:51:26 +0300

    more  range of indexex in python

--- vin.py
+++ vin.py
@@ -3,7 +3,7 @@
 #when specifying a range the return value will be  a  new  list with  specified items
 
 thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
-print(thislist[:4])
+print(thislist[2:])
 #the search will start at index 2 and end at index 5
 
 
