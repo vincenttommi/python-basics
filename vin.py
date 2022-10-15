236d6e45de105b149cc89d5c675ced3dc9c82527
vincenttommi <vincenttommi@gmail.com>
2022-10-15 09:49:03 +0300

    range of indexes

--- vin.py
+++ vin.py
@@ -2,6 +2,8 @@
 #you can specifty range of indexes by speciftying where to start and where to end
 #when specifying a range the return value will be  a  new  list with  specified items
 
-thislist = ["banana","cherry","orange","kiwi","melon","mango"]
-print(thislist[2:5])
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[:4])
+#the search will start at index 2 and end at index 5
+
 
