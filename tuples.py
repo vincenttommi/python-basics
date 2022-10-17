1e2ee7d1f611a70928407127bd23416dbd4fd594
vincenttommi <vincenttommi@gmail.com>
2022-10-17 12:17:32 +0300

    len() in tupple

--- tuples.py
+++ tuples.py
@@ -1,5 +1,6 @@
 #are used to store multiple items in a single variable
 #Is a collection which is ordered and unchangeable
+#len() prints the numbers of length in  tupple
 
 thistuple = (("vincent","kihenjo","tommi","korir"))
-print(thistuple)
+print(len(thistuple))
