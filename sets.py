ab7a62fd19ea640f94b0a1946703f187ecfcbda0
vincenttommi <vincenttommi@gmail.com>
2022-10-17 12:39:10 +0300

    datatypes in sets

--- sets.py
+++ sets.py
@@ -2,6 +2,18 @@
 #are unordered, unchangeable and unindexed
 
 
-worldcup = ["qatar","England","Brazil","ghana","nigeria"]
+# worldcup = ["qatar","England","Brazil","ghana","nigeria"]
 
-print(len(worldcup))
+# print(len(worldcup))
+
+#set items int boolean string
+
+set1  = ["apple","banana","cherry"]
+set2  = [1,2,3,4,5,6,7,8,9,10]
+set3  = {True,False,False}
+
+print(set1)
+print(set2)
+print(set3)
+
+ 
