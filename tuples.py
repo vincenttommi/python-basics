b7cb601ca26792675b0f98065556eb366015c4a3
vincenttommi <vincenttommi@gmail.com>
2022-10-17 12:24:09 +0300

    more tupple in python

--- tuples.py
+++ tuples.py
@@ -1,6 +1,18 @@
-#are used to store multiple items in a single variable
-#Is a collection which is ordered and unchangeable
-#len() prints the numbers of length in  tupple
+# #are used to store multiple items in a single variable
+# #Is a collection which is ordered and unchangeable
+# #len() prints the numbers of length in  tupple
 
-thistuple = (("vincent","kihenjo","tommi","korir"))
-print(len(thistuple))
+
+# name = (("vincent",))
+# print(type(name))
+
+#tupple items
+
+tuple1 = ("apple","banana","cherry")
+tuple2 = (1,2,3,4,5,6,7)
+tuple3 = (True,False,False)
+
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
