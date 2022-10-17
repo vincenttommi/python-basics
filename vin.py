057f459a9582c3c3e638915a2747021ebd42a7e6
vincenttommi <vincenttommi@gmail.com>
2022-10-17 09:46:21 +0300

    customize sort function

--- vin.py
+++ vin.py
@@ -1,7 +1,11 @@
-#sort descending
-#from largest to smallest
+#customize sort function()
+#you can customize your function using key argument key = function
+#The function will return a number that would be used to sort a list
 
+def myfunc(n):
+    return abs(n - 50)
 
-numbers = [45,50,67,80,100,25,27]
-numbers.sort(reverse=True)
+numbers = [100,40,50,80,90,56,78,90,200,300]
+numbers.sort(key = myfunc)
 print(numbers)
+
