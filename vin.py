e56986d99e0a3b002649a87b6b8709f59c974767
vincenttommi <vincenttommi@gmail.com>
2022-10-17 10:05:06 +0300

    casesensitive sort

--- vin.py
+++ vin.py
@@ -1,11 +1,5 @@
-#customize sort function()
-#you can customize your function using key argument key = function
-#The function will return a number that would be used to sort a list
+#caseinsensitive sort
 
-def myfunc(n):
-    return abs(n - 50)
-
-numbers = [100,40,50,80,90,56,78,90,200,300]
-numbers.sort(key = myfunc)
-print(numbers)
-
+thislist = ["banana","orange","kiwi","cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
