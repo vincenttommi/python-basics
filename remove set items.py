6f069f9f3b57d2662630f30c971593b93965114d
vincenttommi <vincenttommi@gmail.com>
2022-10-17 14:12:55 +0300

    clear function in python

--- remove set items.py
+++ remove set items.py
@@ -1,5 +1,30 @@
 #To remove an item from a set we use remove()
+#removing using discard function
+#pop() removes the last item of the sets
 
-thisset = ["apple","banana","cherry","grapes"]
-thisset.remove("banana")
+
+
+# names = {"banana","grapes","mango","beans"}
+
+# x = names.pop()
+# print(x)
+
+# print(names)
+
+
+
+# marks  = {20,50,60,70,80,90}
+
+# x = marks.pop()
+# print(x)
+
+# print(marks)
+
+#clear() function empties the whole set
+
+
+thisset = {"apple","banana","cherry"}
+
+
+thisset.clear()
 print(thisset)
