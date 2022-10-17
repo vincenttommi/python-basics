3e0f1bdef5dd16f13de321b1f4e2fcfa334b3e1e
vincenttommi <vincenttommi@gmail.com>
2022-10-17 12:55:41 +0300

    accessing items in a set

--- sets.py
+++ sets.py
@@ -6,7 +6,7 @@
 
 # print(len(worldcup))
 
-#set items int boolean string
+#set items int boolean stringof 
 
 # set1  = ["apple","banana","cherry"]
 # set2  = [1,2,3,4,5,6,7,8,9,10]
@@ -18,6 +18,24 @@
 
  
  
-myset = ["vincent","tommi","korir"]
-print(type(myset))
+# myset = ["vincent","tommi","korir"]
+# print(type(myset))
  
+ 
+#using a set constructor to make a set
+# thisset = set(("apple","banana","cherry","grapes"))
+
+# print(thisset)
+
+#collection Arrays in python 
+#list
+# dictionary
+#tuple
+# set
+
+#accessing items in python
+
+thisset = {"apple","banana","cherry"}
+
+for x in thisset:
+    print(x)
