a47af1289f92e6ca8e86774d633bce27aa3ae565
vincenttommi <vincenttommi@gmail.com>
2022-10-17 09:31:53 +0300

    sort descending

--- vin.py
+++ vin.py
@@ -1,8 +1,6 @@
-#sorting  list in python
-#are sorted alphabetically and  numerically
-#using  sort()
-#sorts lists alphanumerically 
+#sort descending
+#from largest to smallest
 
-numbers = [20,34,45,67,89,90,100,25]
-numbers.sort()
-print(numbers)
+Teams = ["Brazil","Argentina","France","Norway","Germany"]
+Teams.sort(reverse=True)
+print(Teams)
