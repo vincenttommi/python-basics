e25cc2db0c7e8a850077530b09cd4cd1e0b31cd7
vincenttommi <vincenttommi@gmail.com>
2022-10-17 10:19:10 +0300

    copy() in python

---
+++ copylist.py
@@ -0,0 +1,13 @@
+#copy()
+#using built in function to make a copy in python
+
+
+
+thislist = ["apple","banana","cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+numbers = [20,30,40,50,60,70,80,90,100]
+num2 = numbers.copy()
+print(numbers)
