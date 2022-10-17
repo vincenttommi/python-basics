28dbceb3509521715a34fa808936b9ec152aea15
vincenttommi <vincenttommi@gmail.com>
2022-10-17 13:19:48 +0300

    adding  items in a set

---
+++ add-sets.py
@@ -0,0 +1,27 @@
+# adding items in a set
+# we use add()
+
+
+
+
+# thisset = {"apple","banana","cherry"}
+# thisset.add("orange")
+
+# print(thisset)
+
+#adding set to another function we use  the update method
+
+
+# thisset = {"apple","banana","cherry"}
+# tropical = {"mango","grapes","papaya"}
+
+
+# thisset.update(tropical)
+# print(thisset)
+
+
+marks = {100,200,300,400,500,600,700}
+marks2 = {20,34,50,60,70,80,90}
+
+marks.update(marks2)
+print(marks)
