f88ff8fe434712fd31883924747d8290d774f39b
vincenttommi <vincenttommi@gmail.com>
2022-10-18 12:00:08 +0300

    symmetric_difference()

--- join sets.py
+++ join sets.py
@@ -11,11 +11,45 @@
 #update() inserts items of set1 into set2
 
 
-set1 = {"a","b","c"}
-set2 = {1,2,3}
+# set1 = {"a","b","c","c"}
+# set2 = {1,2,3}
 
 
-set1.update(set2)
+# set1.update(set2)
 
-print(set1)
+# print(set1)
  
+#  the intersection_duplicates
+#keeps items that are  present in  both sets.
+#intersection_update()
+
+
+
+
+# x = {"apple","banana","cherry","google"}
+# y = {"google","microsoft","apple"}
+
+
+# x.intersection_update(y)
+
+# print(x)
+
+# x = {"apple","google","microsoft","cherry","sadiomane"}
+# y = {"apple","sadiomane","cherry","sadiomane"}
+
+
+# x.intersection_update(y)
+
+# print(x)
+ 
+#  Keep All, But NOT the Duplicates
+# symmetric_difference_update() keeps elements that are not both in all sets.
+
+x = {"apple","banana","grapes","mango"}
+y = {"avacado","microsoft","google","twitter"}
+
+
+
+z = x.symmetric_difference(y)
+
+print(x)
