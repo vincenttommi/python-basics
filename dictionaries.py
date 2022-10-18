c40d47ceec27665641537b62243e8cfbfd72c23d
vincenttommi <vincenttommi@gmail.com>
2022-10-18 12:31:22 +0300

    dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+#are used to store data values
+#is a collection which is ordered  changeable and do not allow duplicates
+
+thisdict = {
+    "Team":"Barcelona",
+    "Captain": "Sergio Roberto",
+    "year" : 2022
+     
+}
+
+
+print(thisdict)
