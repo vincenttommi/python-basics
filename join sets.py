1f4a98630385c961f7b865b574b180d994cafa53
vincenttommi <vincenttommi@gmail.com>
2022-10-18 11:32:16 +0300

    joining sets using update()

--- join sets.py
+++ join sets.py
@@ -1,8 +1,21 @@
 #Joining two sets
 #we use union() returns a new set with all items from the set
 
+
+# set1 = {"a","b","c"}
+# set2 = {1,2,3}
+
+# set3 = set1.union(set2)
+# print(set3)
+
+#update() inserts items of set1 into set2
+
+
 set1 = {"a","b","c"}
 set2 = {1,2,3}
 
-set3 = set1.union(set2)
-print(set3)
+
+set1.update(set2)
+
+print(set1)
+ 
