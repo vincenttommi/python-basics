9fc5fe6ac871d69924e25c2573c680565066994d
vincenttommi <vincenttommi@gmail.com>
2022-10-18 12:51:38 +0300

    key() in python

--- accessing dict.py
+++ accessing dict.py
@@ -5,4 +5,18 @@ teams={
     
 }
 
-print (teams[" year "])
+x = teams.keys()
+
+print(x)
+
+teams["continent"]="Europe"
+
+print(teams)  
+# after change
+#before change
+
+
+#keys() returns a list of all keys in the dictionary
+#values() returns a list of all values in the function
+
+
