a1bd692a670d1f8b7b587ee4b89927cfa058ae59
vincenttommi <vincenttommi@gmail.com>
2022-10-20 12:02:33 +0300

    values() in dictionary

--- loop dictionary.py
+++ loop dictionary.py
@@ -1,14 +1,28 @@
 #we use for loop when looping through a dictionary so as it displays items inside the dictionary
 
 
-names = {
+# names = {
     
-    "city1" : "Nairobi",
-    "city2" : "Mombasa",
-    "city3" : "Kisumu",
-    "city4" : "Eldoret",
+#     "city1" : "Nairobi",
+#     "city2" : "Mombasa",
+#     "city3" : "Kisumu",
+#     "city4" : "Eldoret",
     
+# }
+
+# for x in names:
+#     print(names)
+    
+#values() returns values of a dictionary
+
+
+place = {
+    
+    "name" : "Neywork",
+    "continent" : "united states of america",
+    "Population" : "nine billion people",
+    "year" : 2022
 }
 
-for x in names:
-    print(names)
+place.values()
+print(place)
