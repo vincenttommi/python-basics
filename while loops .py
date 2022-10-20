fc2b14013442184859f5f63f781f156ea1de5d02
vincenttommi <vincenttommi@gmail.com>
2022-10-20 15:47:49 +0300

    continue statement in python

--- while loops .py
+++ while loops .py
@@ -10,12 +10,22 @@
     
 #using break statement in while loop
 
-i = 1
-while i < 50:
-  print(i)
-  if (i == 300):
-    break
-  i += 1  
+# i = 1
+# while i < 50:
+#   print(i)
+#   if (i == 300):
+#     break
+#   i += 1  
      
      
  
+#continue statement in python 
+#with continue statement we can  stop the current iteration and continue with the next
+
+i = 0
+while i < 6:
+ i += 1
+ if  i == 3:
+    continue
+print(i)
+
