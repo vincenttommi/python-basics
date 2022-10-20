c47796f78a9fd97484892b7bf9fb7e70d80ec414
vincenttommi <vincenttommi@gmail.com>
2022-10-20 12:55:22 +0300

    elif if in python

--- if else.py
+++ if else.py
@@ -1,7 +1,22 @@
 #python conditions and if  statements
 
-a = 33 
-b = 64
 
-if a < b:
-    print("b is greater than a")
+
+
+# a = 33 
+# b = 64
+
+# if a < b:
+#     print("b is greater than a")
+    
+
+
+#elif in python is a keyword in python way of saying  if the previous conditions were not true,try this condition 
+
+x = 400
+y = 800
+
+if x < y:
+    print("y is greater")
+elif x==y:
+    print("x and are both equal")    
