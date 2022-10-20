23f1f39eae29c669c5b230ca85285dc1ede728f3
vincenttommi <vincenttommi@gmail.com>
2022-10-20 11:41:19 +0300

    removing items from dictionary using pop()

---
+++ add dict items.py
@@ -0,0 +1,1 @@
+#adding a value to a dictionary is done by  using a new index key  and assigning a value to it
---
+++ removing dict items.py
@@ -0,0 +1,15 @@
+#pop() removes items  with specified key name
+
+
+names = {
+    
+    "name" : "kiptoo",
+    "place" : "rongai",
+    "Age"   : "21",
+    "Education" : "Degree",
+}
+
+
+names.pop("Age")
+print(names)
+
