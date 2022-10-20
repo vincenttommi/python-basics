e31963a34f55cc62d049f82f974c09af614c5e1c
vincenttommi <vincenttommi@gmail.com>
2022-10-20 11:21:50 +0300

    changing values in dictionary in python

---
+++ change dict.py
@@ -0,0 +1,16 @@
+#changing values 
+#you can change value of a specific item by reffering to its name
+
+
+Trophies = {
+    
+    "premierLeague":"Mancity",
+    "Bundesliga" : "Bayern Munich",
+    "Spain"   : "Laliga",
+    "Italy" : "Serie A",
+    "France" : "Ligue 1",
+}
+
+Trophies["premierLeague"]="Chelsea"
+
+print(Trophies)
