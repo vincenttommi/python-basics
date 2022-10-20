f6bb334da0326c4cfff12a5fe8c2daa8d1d4339a
vincenttommi <vincenttommi@gmail.com>
2022-10-20 15:39:23 +0300

    using break statement in while loop

--- while loops .py
+++ while loops .py
@@ -2,7 +2,20 @@
 #while loops and for loops
 
 
-i = 10
-while i <  20:
-    i +=1
-    print(i)
+# i = 10
+# while i <  20:
+#     i +=1
+#     print(i)
+    
+    
+#using break statement in while loop
+
+i = 1
+while i < 50:
+  print(i)
+  if (i == 300):
+    break
+  i += 1  
+     
+     
+ 
