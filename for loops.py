55ec89153aedf7fa33e65ad2c7c2a5fde41fd85c
vincenttommi <vincenttommi@gmail.com>
2022-10-21 00:31:08 +0300

    iterating strings using for loop

--- for loops.py
+++ for loops.py
@@ -1,8 +1,15 @@
 #for loops in python
 #a for loop is used in iterating over a sequence
-# either tuple,list , set ,dictionary or string
+# either tuple,list , set ,dictionary or string thus showing output inside values
+#forloop does not require an indexing variable to set beforehand
 
-fruits = ["banana","cherry","grapes","orange"]
+# fruits = ["banana","cherry","grapes","orange"]
 
-for x in fruits:
-    print(fruits)
+# for x in fruits:
+#     print(fruits)
+
+#using for loop to iterate over strings
+
+
+for x in "banana":
+    print(x)
