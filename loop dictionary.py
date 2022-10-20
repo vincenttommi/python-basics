5eaf05d145760d52c9dc5e4903b5bb91622761e4
vincenttommi <vincenttommi@gmail.com>
2022-10-20 11:55:10 +0300

    looping through a dictionary

---
+++ loop dictionary.py
@@ -0,0 +1,14 @@
+#we use for loop when looping through a dictionary so as it displays items inside the dictionary
+
+
+names = {
+    
+    "city1" : "Nairobi",
+    "city2" : "Mombasa",
+    "city3" : "Kisumu",
+    "city4" : "Eldoret",
+    
+}
+
+for x in names:
+    print(names)
