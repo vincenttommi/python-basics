25a99c949ce35a92f0fca28b52c3ba5a6513305b
vincenttommi <vincenttommi@gmail.com>
2022-10-20 12:18:42 +0300

    copying items in dictionary in python

---
+++ copy dictionaries.py
@@ -0,0 +1,11 @@
+# dictionary can be simply be copied by typing dict2 = dixt1
+#copy() used to copy items in a dictionary
+
+thisdict = {
+    "brand" :"Toyota",
+    "Model" : "Isuzu",
+    "year" : 2022
+    
+}
+mydict = thisdict.copy()
+print(mydict)
