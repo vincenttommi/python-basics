036cc18c1f93232aec1a063b793200c76b161e1c
vincenttommi <vincenttommi@gmail.com>
2022-10-22 12:35:46 +0300

    passing arguments in python

--- python functions.py
+++ python functions.py
@@ -5,7 +5,21 @@
 #creating a function in python
 
 
-def myfunction():
-    print('welcome vincenttommi')
+# def myfunction():
+#     print('welcome vincenttommi')
     
-myfunction()    
+# myfunction()    
+
+
+
+#arguments
+#information can be passed into functions as arguments
+#arguments are specified after the function name,inside parenthess.you can add as many arguments as you want
+
+
+def my_function(fname):
+      print(fname + "company")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
