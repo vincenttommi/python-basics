186ae2c7b68fae394135d689a7902bea0b0ee6d8
vincenttommi <vincenttommi@gmail.com>
2022-10-22 15:12:36 +0300

    passing arguments in python

--- python functions.py
+++ python functions.py
@@ -23,13 +23,26 @@
 # my_function("Emil")
 # my_function("Tobias")
 # my_function("Linus")
+#arguments are always shortened to arg in python
 
 
-def my_function(fname):
-    print(fname + "tommi")
+# def my_function(fname):
+#     print(fname + "tommi")
 
     
     
-my_function("vincent")
-my_function("vin")
-my_function("korir")
+# my_function("vincent")
+# my_function("vin")
+# my_function("korir")
+
+#By default a function must be called with exact number of arguments
+#meaning if your function expects 2 arguments you have to call to functions with two argumnets
+
+
+
+    
+
+def my_function(fname,lname):
+    print(fname +  "" + lname)
+    
+    my_function("vincent", "tommi")
