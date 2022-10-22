9c5765c67df9a0cdada49800f138857d63114256
vincenttommi <vincenttommi@gmail.com>
2022-10-22 12:21:30 +0300

    creating a function in python

---
+++ python functions.py
@@ -0,0 +1,12 @@
+#function is a block of code that which runs when called.
+#you can pass data,known as parameters,into function
+
+
+#creating a function in python
+
+
+def my_function():
+    print("hello from a function")
+    
+    
+my_function()    
