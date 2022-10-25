aa1c05b77e9b2c1255837e71d7b00f497c10e2ba
vincenttommi <vincenttommi@gmail.com>
2022-10-25 08:37:49 +0300

    more clasess in python

--- classes.py
+++ classes.py
@@ -1,5 +1,13 @@
 #python classess and objects
+#a class is alike an object constructor
+# class myclass:
+#     x = 5
+# print(myclass)    
+
+
 
 class myclass:
-    x = 5
-print(myclass)    
+    x=5
+    
+p1 = myclass()
+print(p1.x)    
