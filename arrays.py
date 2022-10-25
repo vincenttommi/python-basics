c62053e1d8a93665e2c5b3f5b24d9de3c06ee4fb
vincenttommi <vincenttommi@gmail.com>
2022-10-25 07:40:06 +0300

    accessing elements in array

--- arrays.py
+++ arrays.py
@@ -1,6 +1,8 @@
 #python does not have built in  support for arrays but has lists instead
+#array is a special value which can hold more than one value at a time
+#accessing elements in array
 
 cars = ["BMW","MerceBenz","Volvo","pequot"]
 
-for x in cars:
-    print(x)
+x = cars[3]
+print(x)
