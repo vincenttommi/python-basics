b8fe4f6423089a1cf3ec1199774d2344f1918978
vincenttommi <vincenttommi@gmail.com>
2022-10-25 10:36:22 +0300

    strings are iterable objects in python

---
+++ iterator.py
@@ -0,0 +1,25 @@
+#is an object that contains number of countable  values
+#an iterator is an onject that can iterated upon
+#uses the following method _ter_() and _next_()
+
+
+
+
+# myscores = (20,30,40,50,60,70,80)
+# myit = iter(myscores)
+
+# print(next(myit))
+# print(next(myit))
+# print(next(myit))
+
+
+#strings are iterable objects and can return  an iterator
+
+
+mystr = "banana"
+myit = iter(mystr)
+
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
