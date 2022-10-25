a5cb813e993c535fa20d64e86e8a31de06cfe644
vincenttommi <vincenttommi@gmail.com>
2022-10-25 07:28:34 +0300

    lambda function in python

--- lamba function.py
+++ lamba function.py
@@ -15,9 +15,20 @@
 # x = lambda a,b: a*b
 # print(x(10,20))
 
-x = lambda a,b:a-b
-print(x(5,6))
+# x = lambda a,b:a-b
+# print(x(5,6))
 
 
-x = lambda a,b,c:a*b*c
-print(x(23,45,56))
+# x = lambda a,b,c:a*b*c
+# print(x(23,45,56))
+
+#lambda function
+
+def myfunc(n):
+    return lambda a : a * n
+
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
+ 
+ 
