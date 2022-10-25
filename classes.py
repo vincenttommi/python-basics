c57be008ee853dab862d1b3842ad20fdfd156de0
vincenttommi <vincenttommi@gmail.com>
2022-10-25 09:35:10 +0300

    inheritance in python

--- classes.py
+++ classes.py
@@ -6,8 +6,23 @@
 
 
 
-class myclass:
-    x=5
+# class myclass:
+#     x=5
     
-p1 = myclass()
-print(p1.x)    
+# p1 = myclass()
+# print(p1.x)    
+
+
+# The_str_()function
+#controls what should be returned  when the class object is  reprsented as a string
+#the string_str_function is not set but  is returned
+
+
+class person:
+    def _init_(self,name,age):
+        self.name=name
+        self.age=age
+
+
+p1 = person("john",36)
+print(p1)
