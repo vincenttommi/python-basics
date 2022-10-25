57192ba13d2070dd2b035888ffd6ba485a866b7a
vincenttommi <vincenttommi@gmail.com>
2022-10-25 07:09:23 +0300

    lamdba fucntion in python

---
+++ lamba function.py
@@ -0,0 +1,9 @@
+#lambda function in python
+# is a small anonymous function
+#can take any number of argumnents but can only take one expression
+
+#syntax
+#lambda arguments:expression
+
+x = lambda a: a + 10
+print(x(5))
