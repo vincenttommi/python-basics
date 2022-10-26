f8293a09f00d8423df12bc22a0a81dd822de1e2a
vincenttommi <vincenttommi@gmail.com>
2022-10-26 13:36:39 +0300

    modules in python

---
+++ modules.py
@@ -0,0 +1,5 @@
+import mymodule
+
+
+mymodule.greetings("vincent")
+
---
+++ mymodule.py
@@ -0,0 +1,8 @@
+#modules in python 
+#modules are sets of  functions that you want to include in your application
+
+
+def greetings(name):
+    
+    print("Hello" + name)
+    
