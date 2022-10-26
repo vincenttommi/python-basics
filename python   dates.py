a93fa44bc9068bd8853cf46caba067be2de37cc7
vincenttommi <vincenttommi@gmail.com>
2022-10-26 15:09:41 +0300

    creating date objects in python

--- python   dates.py
+++ python   dates.py
@@ -2,20 +2,35 @@
 # dates as objects
 
 
+# import datetime
+
+# x = datetime.datetime.now()
+
+
+# print(x)
+
+
+
+# #returning year and name of the week
+
+# import datetime
+
+# x = datetime.datetime.now()
+# print(x.year)
+
+# print(x.strftime("%A"))
+
+
+#creating date objects in python
+
+
+#To create a date  wes uses datetime() constructor
+#datetime() requires three parameters to create a date:year,month,day
+
+
+
 import datetime
 
-x = datetime.datetime.now()
-
+x = datetime.datetime(2022,10,26)
 
 print(x)
-
-
-
-#returning year and name of the week
-
-import datetime
-
-x = datetime.datetime.now()
-print(x.year)
-
-print(x.strftime("%A"))
