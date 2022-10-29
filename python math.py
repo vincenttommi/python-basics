a51f2e82e42f7d985241ed9f1d98ec5363f48a83
vincenttommi <vincenttommi@gmail.com>
2022-10-29 11:00:21 +0300

    absollute() in python

--- python math.py
+++ python math.py
@@ -3,10 +3,18 @@
 #min() and max() can bes used to find lowest or highest value in an iterable
 
 
-x = min(5,10,20)
-y = max(5,10,20)
+# x = min(5,10,20)
+# y = max(5,10,20)
 
 
+
+# print(x)
+# print(y)
+
+
+#absolute function in python()
+#returns absolute value of the specificied number
+
+x = abs(-7.25)
 
 print(x)
-print(y)
