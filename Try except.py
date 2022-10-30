da999c28712e2de10d7ac02c3b089a6605e8808d
vincenttommi <vincenttommi@gmail.com>
2022-10-30 10:13:44 +0300

    more exception handling in python

--- Try except.py
+++ Try except.py
@@ -5,10 +5,40 @@
 
 
 
+
+
+
+# try:
+    
+#     print(x)
+# except NameError:
+#     print("variable x is not defined")
+# except:
+#     print("something else went wrong")    
+
+
+
+#else you can use the else keyword to define the block of code to be excuted
+
+
+# try:
+#     print("hello world")
+# except:
+#     print("something went wrong")
+# else:
+#     print("nothing went wrong")    
+    
+    
+    
+#try block does not generate an error
+
+#finally block
+#if specified will be excuted regardless if the try block raises an  error
 try:
     print(x)
 except:
-    print("An exception  occured")    
+    print("something went wrong") 
     
-    
-    
+finally:
+    print("The try except is finished")
+        
