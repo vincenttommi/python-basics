c974e2b2e036ff76602e0c2d2c68fb98956cea4e
vincenttommi <vincenttommi@gmail.com>
2022-10-30 13:19:44 +0300

    raising a typeerror

--- Try except.py
+++ Try except.py
@@ -66,6 +66,15 @@
 #raising exception
 #raise an error and stop a program if x is lower than 0
 
-x = -1
-if x < 0:
-    raise Exception("sorry,no numbers below zero")
+# x = -1
+# if x < 0:
+#     raise Exception("sorry,no numbers below zero")
+
+
+
+
+#raising a TypeError if x is not an integer
+x = "hello"
+if not type(x) is int:
+    raise TypeError("only integers are allowed")
+
