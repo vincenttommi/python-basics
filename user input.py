de796b1eedbbf0b4051452f4094f7ccef9ba5fb7
vincenttommi <vincenttommi@gmail.com>
2022-10-31 10:33:06 +0300

    user input in python

---
+++ user input.py
@@ -0,0 +1,15 @@
+#pythonn inputs
+#uses input()
+#in python 3.6
+
+username = input("Enter username:")
+print("username is :" +username)
+ 
+
+#user input in python 2.6
+
+
+
+
+names = input("Enter your names nigga:")
+print("username is :" + username)
