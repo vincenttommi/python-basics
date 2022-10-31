8932f24a3f0fb1f27ecb2c4c3e99fe9d1cb8b02f
vincenttommi <vincenttommi@gmail.com>
2022-10-31 11:03:53 +0300

    index numbers in python

--- string formatting.py
+++ string formatting.py
@@ -14,12 +14,21 @@
 #if you want to use  more value just add  more values to format() method
 
 
-quantity = 3
+# quantity = 3
+# itemno = 567
+# price = 49 
+
+# myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+# print(myorder.format(quantity,itemno,price))
+
+
+
+
+#index numbers in python
+
+quantity =  400
 itemno = 567
-price = 49 
+price = 50
 
-myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+myorder = "I want {0}  pieces of  item number {1} for {2:.2f} dollars"
 print(myorder.format(quantity,itemno,price))
-
-
-
