1c454a9df53364c9641bb60e1b0bcfaaffe3b01f
vincenttommi <vincenttommi@gmail.com>
2022-10-31 10:57:27 +0300

    more multiple values using string format

--- string formatting.py
+++ string formatting.py
@@ -2,8 +2,24 @@
 #allows one to format selected parts of a string
 
 
-meangrade=" 70.0"
-txt = "your mean grade is {} credit"
+# meangrade=" 70.0"
+# txt = "your mean grade is {} credit"
 
-print(txt.format(meangrade))
+# print(txt.format(meangrade))
+
+
+
+
+#multiple values 
+#if you want to use  more value just add  more values to format() method
+
+
+quantity = 3
+itemno = 567
+price = 49 
+
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity,itemno,price))
+
+
 
